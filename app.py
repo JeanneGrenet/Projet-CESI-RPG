1b0d2ffb98d992e82b40ade17b989a23ea5e0e4d
Jeanne Grenet <jeanne.grent@gmail.com>
2023-12-20 13:36:58 +0100

    add the 2 pages html + modif in the player code

--- app.py
+++ app.py
@@ -1,11 +1,13 @@
 import flask
 import sqlite3
 
+
+
 app = flask.Flask(__name__, template_folder='views')
 
 @app.route('/')
 def home():
-   connection = sqlite3.connect('Projet-CESI-RPG/rpg.db')
+   connection = sqlite3.connect('src/rpg.db')
    cursor = connection.cursor()
    cursor.execute('SELECT * FROM User')
    users = cursor.fetchall()
@@ -21,8 +23,89 @@ def home():
          "attackPoints": user[3],
          "defensePoints": user[4],
          "speedPoints": user[5],
+         "date": user[6],
 
       }) 
 
-   return flask.render_template('index.html', uses=list_users)
+   return flask.render_template('index.html', users=list_users)
 
+@app.route('/monsters')
+def monsters():
+   connection = sqlite3.connect('src/rpg.db')
+   cursor = connection.cursor()
+   cursor.execute('SELECT * FROM monsters ORDER BY attackPoints')
+   monsters = cursor.fetchall()
+   connection.close()
+   
+   list_monsters = []
+
+   for monster in monsters:
+      list_monsters.append({
+         "id": monster[0],
+         "type": monster[1],
+         "attackPoints": monster[2],
+         "defensePoints": monster[3],
+         "speedPoints": monster[4],
+
+      }) 
+
+   return flask.render_template('monsters.html', monsters=list_monsters)
+
+@app.route('/add', methods=['GET', 'POST'])
+def add():
+
+    #on vérifie le protocole HTML de la requête
+    if flask.request.method == 'POST':
+
+        #Si c'est POST, on récupère les valeurs du formulaire
+        typeMonster = flask.request.values.get('type')
+        attackPoints = flask.request.values.get('attackPoints')
+        defensePoints = flask.request.values.get('defensePoints')
+        speedPoints = flask.request.values.get('speedPoints')
+
+        #On se connecte à la base de données et on INSERT notre nouveau chien !
+        connection = sqlite3.connect('src/rpg.db')
+
+        cursor = connection.cursor()
+        cursor.execute('INSERT INTO monsters (type, attackPoints, defensePoints, speedPoints) VALUES ("' + typeMonster + '", "' + attackPoints + '", "' + defensePoints + '", "' + speedPoints + '")')
+        connection.commit()
+        connection.close()
+
+        #Quand c'est bon, on redirige vers la page d'accueil
+        return flask.redirect('/monsters')
+    else:
+
+        #Si ce n'est pas une requête POST, on affiche le formulaire
+        return flask.render_template('add.html')
+
+@app.route('/deleteMonster/<id>')
+#On prend en paramètre l'id de la donnée qu'on veut suppriemr
+def deleteMonster(id):
+    #On se connecte à la base de données
+    connection = sqlite3.connect('src/rpg.db')
+
+    cursor = connection.cursor()
+
+    #On supprime la donnée si l'ID est égal au paramètre
+    cursor.execute('DELETE FROM monsters WHERE id = ' + id)
+    connection.commit()
+    connection.close()
+
+    #On redirige l'utilisateur sur la page d'accueil
+    return flask.redirect('/monsters')
+
+@app.route('/deleteUser/<id>')
+#On prend en paramètre l'id de la donnée qu'on veut suppriemr
+def deleteUser(id):
+    #On se connecte à la base de données
+    connection = sqlite3.connect('src/rpg.db')
+
+    cursor = connection.cursor()
+
+    #On supprime la donnée si l'ID est égal au paramètre
+    cursor.execute('DELETE FROM User WHERE id = ' + id)
+    connection.commit()
+    connection.close()
+
+    #On redirige l'utilisateur sur la page d'accueil
+    return flask.redirect('/')
--- src/dbManagment.py
+++ src/dbManagment.py
@@ -1,63 +1,10 @@
 import sqlite3
 
-def loadTables():
-    connection = sqlite3.connect('Projet-CESI-RPG/rpg.db')
-    cursor = connection.cursor()
-    cursor.execute("""
-        CREATE TABLE IF NOT EXISTS monsters(
-            id INTEGER PRIMARY KEY AUTOINCREMENT UNIQUE,
-            type TEXT,
-            attackPoints INTERGER,
-            defensePoints INTERGER,
-            speedPoints INTEGER
-        )
-        """)
-    connection.commit()
-    connection.close()
 
-    connection = sqlite3.connect('Projet-CESI-RPG/rpg.db')
-    cursor = connection.cursor()
-    cursor.execute("""
-        CREATE TABLE IF NOT EXISTS weapons(
-            id INTEGER PRIMARY KEY AUTOINCREMENT UNIQUE,
-            name TEXT,
-            attackMultiplier INTERGER,
-            speedMultiplier INTEGER
-        )
-        """)
-    connection.commit()
-    connection.close()
-
-    connection = sqlite3.connect('Projet-CESI-RPG/rpg.db')
-    cursor = connection.cursor()
-    cursor.execute("""
-        CREATE TABLE IF NOT EXISTS equipments(
-            id INTEGER PRIMARY KEY AUTOINCREMENT UNIQUE,
-            name TEXT,
-            defenseMultiplier INTERGER,
-            speedMultiplier INTEGER
-        )
-        """)
-    connection.commit()
-    connection.close()
-
-    monsterList = ["Squelette", "Loup-Garou", "Gobelin", "Orc", "Troll", "Ogre", "Dragon", "Chimère", "Vampire", "Hydre"]
-    weaponList = [ "Batton", "Hâche", "Marteau", "Poignard", "Épée", "Lame du Héro" ]
-    equipmentList= [ ["Casque", 3, 3], ["Plastron", 5, 2], ["Bouclier", 4, 1], ["Gant", 1, 5], ["Bottes",2, 4], ["Armure du Héro", 10, 10] ]
-
-
-    for i in range(len(monsterList)):
-        addMonster((monsterList[i], i+2, i+1, 15-i))
-        
-    for i in range(len(weaponList)):
-        addWeapon((weaponList[i], i+2, i+4))
-        
-    for i in range(len(equipmentList)):
-        addEquipment((equipmentList[i][0], equipmentList[i][1], equipmentList[i][2]))
 
 
 def addMonster(monster): 
-    connection = sqlite3.connect('Projet-CESI-RPG/rpg.db')
+    connection = sqlite3.connect('src/rpg.db')
     cursor = connection.cursor()
     cursor.execute("""
         INSERT INTO monsters(type, attackPoints, defensePoints, speedPoints) VALUES(?,?,?,?)
@@ -66,7 +13,7 @@ def addMonster(monster):
     connection.close()
     
 def addWeapon(weapon): 
-    connection = sqlite3.connect('Projet-CESI-RPG/rpg.db')
+    connection = sqlite3.connect('src/rpg.db')
     cursor = connection.cursor()
     cursor.execute("""
         INSERT INTO weapons(name, attackMultiplier, speedMultiplier) VALUES(?,?,?)
@@ -75,7 +22,7 @@ def addWeapon(weapon):
     connection.close()
     
 def addEquipment(equipment): 
-    connection = sqlite3.connect('Projet-CESI-RPG/rpg.db')
+    connection = sqlite3.connect('src/rpg.db')
     cursor = connection.cursor()
     cursor.execute("""
         INSERT INTO equipments(name, defenseMultiplier, speedMultiplier) VALUES(?,?,?)
@@ -84,7 +31,7 @@ def addEquipment(equipment):
     connection.close()
         
 def deletTables():
-    connection = sqlite3.connect('Projet-CESI-RPG/rpg.db')
+    connection = sqlite3.connect('src/rpg.db')
     cursor = connection.cursor()
     cursor.execute("""
         DROP TABLE monsters
@@ -99,7 +46,7 @@ def deletTables():
     connection.close()
         
 def allWeapons():
-    connection = sqlite3.connect('Projet-CESI-RPG/rpg.db')
+    connection = sqlite3.connect('rpg.db')
     cursor = connection.cursor()
     cursor.execute("""SELECT * FROM weapons""")
     weapons = cursor.fetchall()
@@ -107,7 +54,7 @@ def allWeapons():
     return weapons
 
 def allEquipments():
-    connection = sqlite3.connect('Projet-CESI-RPG/rpg.db')
+    connection = sqlite3.connect('rpg.db')
     cursor = connection.cursor()
     cursor.execute("""SELECT * FROM equipments""")
     equipments = cursor.fetchall()
@@ -115,35 +62,93 @@ def allEquipments():
     return equipments
 
 def allMonsters():
-    connection = sqlite3.connect('Projet-CESI-RPG/rpg.db')
+    connection = sqlite3.connect('rpg.db')
     cursor = connection.cursor()
-    cursor.execute("""SELECT * FROM monsters""")
+    cursor.execute("""SELECT * FROM monsters ORDER BY attackPoints""")
     monsters = cursor.fetchall()
     connection.close()
     return monsters
 
 def addUser(user):
-    connection = sqlite3.connect('Projet-CESI-RPG/rpg.db')
+    connection = sqlite3.connect('rpg.db')
     cursor = connection.cursor()
     cursor.execute("""
-        INSERT INTO User(name, level, attackPoints, defensePoints, speedPoints) VALUES(?,?,?,?,?)
+        INSERT INTO User(name, level, attackPoints, defensePoints, speedPoints, date) VALUES(?,?,?,?,?,?)
     """, user)
     connection.commit()
     connection.close()
 
 #Create the table to save users
 
-connection = sqlite3.connect('Projet-CESI-RPG/rpg.db')
-cursor = connection.cursor()
-cursor.execute("""
-    CREATE TABLE IF NOT EXISTS User(
-        id INTEGER PRIMARY KEY AUTOINCREMENT UNIQUE,
-        name TEXT,
-        level INTERGER,
-        attackPoints INTEGER,
-        defensePoints INTERGER,
-        speedPoints INTEGER
-    )
-    """)
-connection.commit()
-connection.close()
+# connection = sqlite3.connect('src/rpg.db')
+# cursor = connection.cursor()
+# cursor.execute("""
+#     CREATE TABLE IF NOT EXISTS User(
+#         id INTEGER PRIMARY KEY AUTOINCREMENT UNIQUE,
+#         name TEXT,
+#         level INTERGER,
+#         attackPoints INTEGER,
+#         defensePoints INTERGER,
+#         speedPoints INTEGER
+#     )
+#     """)
+# connection.commit()
+# connection.close()
+
+
+# connection = sqlite3.connect('src/rpg.db')
+# cursor = connection.cursor()
+# cursor.execute("""
+#     CREATE TABLE IF NOT EXISTS monsters(
+#         id INTEGER PRIMARY KEY AUTOINCREMENT UNIQUE,
+#         type TEXT,
+#         attackPoints INTERGER,
+#         defensePoints INTERGER,
+#         speedPoints INTEGER
+#     )
+#     """)
+# connection.commit()
+# connection.close()
+
+# connection = sqlite3.connect('src/rpg.db')
+# cursor = connection.cursor()
+# cursor.execute("""
+#     CREATE TABLE IF NOT EXISTS weapons(
+#         id INTEGER PRIMARY KEY AUTOINCREMENT UNIQUE,
+#         name TEXT,
+#         attackMultiplier INTERGER,
+#         speedMultiplier INTEGER
+#     )
+#     """)
+# connection.commit()
+# connection.close()
+
+# connection = sqlite3.connect('src/rpg.db')
+# cursor = connection.cursor()
+# cursor.execute("""
+#     CREATE TABLE IF NOT EXISTS equipments(
+#         id INTEGER PRIMARY KEY AUTOINCREMENT UNIQUE,
+#         name TEXT,
+#         defenseMultiplier INTERGER,
+#         speedMultiplier INTEGER
+#     )
+#     """)
+# connection.commit()
+# connection.close()
+
+# monsterList = ["Serpent","Squelette", "Loup-Garou", "Gobelin", "Orc", "Troll", "Ogre", "Dragon", "Chimère", "Vampire", "Hydre"]
+# weaponList = [ "Batton", "Hâche", "Marteau", "Poignard", "Épée", "Lame du Héro" ]
+# equipmentList= [ ["Casque", 3, 3], ["Plastron", 5, 2], ["Bouclier", 4, 1], ["Gant", 1, 5], ["Bottes",2, 4], ["Armure du Héro", 10, 10] ]
+
+# for i in range(len(monsterList)):
+#     addMonster((monsterList[i], i+2, i+1, 15-i))
+    
+# for i in range(len(weaponList)):
+#     addWeapon((weaponList[i], i+2, i+4))
+    
+# for i in range(len(equipmentList)):
+#     addEquipment((equipmentList[i][0], equipmentList[i][1], equipmentList[i][2]))
+
+
+
+
